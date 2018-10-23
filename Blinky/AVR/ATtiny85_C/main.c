2712481626bbe0c0a9165dd6880f8421eb2a4e29
Thee Vanichangkul <thee.vanichangkul@siemens.com>
2018-10-23 16:11:57 +0200

    Initial commit

---
+++ Blinky/AVR/ATtiny85_C/main.c
@@ -0,0 +1,24 @@
+/*
+ * blinky.c
+ *
+ * Created: 22-Apr-18 4:59:34 PM
+ * Author : Thee
+ */ 
+//#define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	DDRB |= 0x02;
+
+    while (1) 
+    {
+		PORTB |= 0x02;
+		_delay_ms(500);
+		PORTB &= ~(0x02);
+		_delay_ms(100);
+    }
+}
+
